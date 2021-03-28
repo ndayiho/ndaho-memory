ea063fd3861eb715487dafa7af47d6c94b958be2
ndayiho <ndayiho5@gmail.com>
2021-03-28 23:20:51 +0200

    intro manipulez des listes de composants

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import shuffle from 'lodash.shuffle'
 
-function App() {
+import './App.css'
+
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+
+const generateCards = () => {
+  const result = []
+  const size = SIDE * SIDE
+  const candidates = shuffle(SYMBOLS)
+  while (result.length < size) {
+    const card = candidates.pop()
+    result.push(card, card)
+  }
+  return shuffle(result)
+}
+
+
+const App = () => {
+
+  const cards = generateCards();
+
+  const handleCardClick = cardValue => {
+    console.log(`click on  ${cardValue}`)
+  }
+
+  const won = new Date().getSeconds() % 2 === 0
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="memory">
+      <GuessCount guesses={0} />
+      <Card card="😀" feedback="hidden" onClick={handleCardClick} />
+      <Card card="🎉" feedback="justMatched" onClick={handleCardClick} />
+      <Card
+        card="💖"
+        feedback="justMismatched"
+        onClick={handleCardClick}
+      />
+      <Card card="🎩" feedback="visible" onClick={handleCardClick} />
+      <Card card="🐶" feedback="hidden" onClick={handleCardClick} />
+      <Card card="🐱" feedback="justMatched" onClick={handleCardClick} />
+      {won && <p>GAGNÉ !</p>}
     </div>
-  );
+  )
 }
 
 export default App;
